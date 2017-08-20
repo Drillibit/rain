6e6b817d4a16deba6d25bdbc79f85e8839f7bd19
SergeBuketov <bukenoff.s@gmail.com>
2017-08-20 15:50:09 +0600

    commited rain2

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 window.addEventListener("load", function(){
   var count = 20;
   for(var i = 0; i < count; i++){
-    var node = document.
-    createElement("span");
+    var node = document.createElement("span");
     node.style.left = Math.floor(Math.random() * 100)+"vw";
     node.style.top = Math.floor(Math.random() * 100)+"vh";
     document.querySelector(".rain").appendChild(node);
+    document.querySelector(".rain2").appendChild(node);
   }
+  
+    
 });
