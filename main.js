aa788990c6a399286b204b8347770b566f70b587
Drillibit <293344asd@gmail.com>
2017-08-20 11:20:43 +0600

    rain animation

---
+++ main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+  var count = 20;
+  for(var i = 0; i < count; i++){
+    var node = document.
+    createElement("span");
+    node.style.left = Math.floor(Math.random() * 100)+"vw";
+    node.style.top = Math.floor(Math.random() * 100)+"vh";
+    document.querySelector(".rain").appendChild(node);
+  }
+});
